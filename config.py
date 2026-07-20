438c67c57ea1b68c5ec0bd8c5a823f6872494ce4
Nahyara Miranda <nmiranda.tech@gmail.com>
2026-07-20 16:20:17 -0300

    implementa extracao e carga da camada raw

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ ANO = "2025"
 # ID do arquivo no Google Drive:
 # Como obter: no Drive clique em "Compartilhar" -> "Qualquer pessoa com o link";
 # o link fica .../file/d/ESTE_TRECHO_E_O_ID/view -> copie o ID e cole abaixo.
-DRIVE_FILE_ID = "COLE_AQUI_O_ID_DO_ARQUIVO_NO_DRIVE"
+DRIVE_FILE_ID = "15vGhmvT0Ux2crqHy_YeRoRiaiCkdB88A"
 
 # Tamanho do bloco de leitura/insercao (numero de linhas por vez).
 # Ler tudo de uma vez estouraria a memoria; por isso lemos em "pedacos".
